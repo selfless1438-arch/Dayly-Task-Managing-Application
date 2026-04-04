ac2170baba55b9885310dec5f1401ee8fb28d118
Danish Ameer <selfless1438@gmail.com>
2026-04-04 10:20:41 +0500

    Destroy Cookies On Logout

--- dashboard/logout.php
+++ dashboard/logout.php
@@ -2,5 +2,9 @@
 session_start();
 session_unset();
 session_destroy();
+if (isset($_COOKIE['username'])) {
+    setcookie("username", "", time() - 3600, "/");
+}
+
 header("Location: login.php");
 exit;
