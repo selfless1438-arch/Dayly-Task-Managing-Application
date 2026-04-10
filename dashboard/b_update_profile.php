8182d824fed25f12c522d325e6aab91132ff507d
Danish Ameer <selfless1438@gmail.com>
2026-04-10 03:06:59 -0700

    profile update pending

--- dashboard/b_update_profile.php
+++ dashboard/b_update_profile.php
@@ -2,6 +2,12 @@
 include 'db.php';
 header("Content-Type: application/json");
 
-$
+$username = $_POST['username'];
+$fullname = $_POST['fullname'];
+$contact = $_POST['contact'];
+$gender = $_POST['gender'];
+$birth_date = $_POST['birth_date'];
+$address = $_POST['address'];
+
 
 ?>
